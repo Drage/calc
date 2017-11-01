4689e15ebd2c909fd069bae9f981918a09aa4ab9
Matt Drage <6196285+Drage@users.noreply.github.com>
2017-11-01 18:56:23 +0800

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -12,8 +12,6 @@ using namespace std;
 enum Associativity { LEFT, RIGHT, NONE };
 const int MAX_ARGS = 2;
 typedef double (*EvalFunc)(double*);
-double x = 0;
-typedef double (*EqFunc)(double, vector<string>, vector<string>);
 
 struct Operator
 {
@@ -55,6 +53,7 @@ double Subtract(double* args) { return args[1] - args[0]; }
 double Multiply(double* args) { return args[1] * args[0]; }
 double Divide(double* args) { return args[1] / args[0]; }
 double Power(double* args) { return pow(args[1], args[0]); }
+double Factorial(double* args) { double f = 1; for(int i = 1; i <= args[0]; ++i) f *= i; return f; }
 double Sin(double* args) { return sin(args[0]); }
 double Cos(double* args) { return cos(args[0]); }
 double Tan(double* args) { return tan(args[0]); }
@@ -67,7 +66,6 @@ double Log(double* args) { return log10(args[0]); }
 double Ln(double* args) { return log(args[0]); }
 double Pi(double* args) { return M_PI; }
 double E(double* args) { return M_E; }
-double X(double* args) { return x; }
 
 double GetLastResult(double* args)
 {
@@ -83,7 +81,7 @@ double GetLastResult(double* args)
 		return lastResult;
 	}
 	else
-		throw string("previous result could not be loaded");
+		throw "previous result could not be loaded";
 }
 
 void SetLastResult(double result)
@@ -98,7 +96,7 @@ void SetLastResult(double result)
 		file.close();
 	}
 	else
-		throw string("could not save result");
+		throw "could not save result";
 }
 
 void InitOperators()
@@ -109,6 +107,7 @@ void InitOperators()
 	operators.push_back(new Operator("*", 2, LEFT, 3, Multiply));
 	operators.push_back(new Operator("/", 2, LEFT, 3, Divide));
 	operators.push_back(new Operator("^", 2, RIGHT, 4, Power));
+	operators.push_back(new Operator("!", 1, LEFT, 6, Factorial));
 	
 	// Functions
 	operators.push_back(new Operator("sin", 1, NONE, 5, Sin));
@@ -126,7 +125,7 @@ void InitOperators()
 	operators.push_back(new Operator("pi", 0, NONE, 5, Pi));
 	operators.push_back(new Operator("e", 0, NONE, 5, E));
 	operators.push_back(new Operator("ans", 0, NONE, 5, GetLastResult));
-	operators.push_back(new Operator("x", 0, NONE, 5, X));
+	operators.push_back(new Operator("x", 0, NONE, 5, NULL));
 	
 	// Brackets
 	operators.push_back(new Operator("(", 0, NONE, 0, NULL));
@@ -156,24 +155,18 @@ void Tokenise(string expression, vector<string> &tokens)
 			while (funcEnd < expression.length() && isalpha(expression[funcEnd]))
 				funcEnd++;
 			
-			// Check function is a legit operator (sin, cos, tan, sqrt or pi)
+			// Check function is a valid operator and add to token list
 			if (IsOperator(expression.substr(0, funcEnd)))
 			{
-				// Add function to token list and remove from expression
 				tokens.push_back(expression.substr(0, funcEnd));
 				expression = expression.substr(funcEnd);
 			}
 			else
-			{
-				stringstream ss;
-				ss << "invalid function '" << expression.substr(0, funcEnd) << "'";
-				throw ss.str();
-			}
+				throw "invalid function '" + expression.substr(0, funcEnd) + "'";
 		}
 		else if (IsOperator(expression.substr(0, 1)))
 		{
 			// Add operator to token list and remove from expression
-			// (All operators which are not functions are 1 character)
 			tokens.push_back(expression.substr(0, 1));
 			expression = expression.substr(1);
 		}
@@ -185,17 +178,15 @@ void Tokenise(string expression, vector<string> &tokens)
 		else
 		{
 			// Unknown character
-			stringstream ss;
-			ss << "invalid character '" << expression[0] << "'";
-			throw ss.str();
+			throw "invalid character '" + expression.substr(1) + "'";
 		}
 	}
 	
 	// Handle negative numbers
-	// For any sequence [operator, '-', number] make the number -ve and remove the '-' operator
+	// For any sequence [operator, '-', number] make the number negative and remove the '-' operator
 	for (int i = 0; i < (int)tokens.size() - 2; i++)
 	{
-		if ((tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+		if ((tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/" || tokens[i] == "^") 
 			&& tokens[i+1] == "-" && isdigit(tokens[i+2][0]))
 		{
 			tokens[i+2] = "-" + tokens[i+2];
@@ -212,15 +203,13 @@ void Tokenise(string expression, vector<string> &tokens)
 		}
 	}
 	
-	// Handle implicit multiplication
-	// E.g. 2(4-1) should be changed to 2*(4-1)
+	// Handle implicit multiplication e.g. 2(4-1) should be changed to 2*(4-1)
 	for (int i = 0; i < (int)tokens.size() - 1; i++)
 	{
 		if (isdigit(*tokens[i].rbegin()) && (tokens[i+1] == "(" || tokens[i+1] == "x"))
 			tokens.insert(tokens.begin()+i+1, "*");
 	}
-	// Special case where first token is ')' and second is 'x'
-	// i.e. (1/2)x
+	// Special case where first token is ')' and second is 'x' e.g. (1/2)x
 	for (int i = 0; i < (int)tokens.size() - 1; i++)
 	{
 		if (tokens[i] == ")" && tokens[i+1] == "x")
@@ -288,7 +277,7 @@ void ConvertToRPN(vector<string> &infix, vector<string> &rpn)
 }
 
 // Evaluate an expression in RPN form
-double Evaluate(vector<string> &tokens)
+double Evaluate(vector<string> &tokens, double x = 0)
 {
 	stack<double> rpnStack;
 	for (unsigned i = 0; i < tokens.size(); i++)
@@ -307,7 +296,7 @@ double Evaluate(vector<string> &tokens)
 			
 			// Check that there are sufficient operands on the stack
 			if ((int)rpnStack.size() < op->numArgs)
-				throw string("failed to evaluate expression");
+				throw "failed to evaluate expression";
 			
 			// Create args list
 			double args[MAX_ARGS];
@@ -317,32 +306,29 @@ double Evaluate(vector<string> &tokens)
 				rpnStack.pop();
 			}
 			
-			// Evaluate operator
-			double result = op->evaluate(args);
-			
-			// Put result back on stack
+			// Evaluate operator and push result back on stack
+			double result = op->value == "x" ? x : op->evaluate(args);
 			rpnStack.push(result);
 		}
 	}
 	
 	// Check only one number is left on the stack
-	if (rpnStack.size() != 1)
-		throw string("failed to evaluate expression");
+	if (rpnStack.size() != 1) 
+		throw "failed to evaluate expression";
 	
 	return rpnStack.top();
 }
 
 // Implementation of Brent's algorithm for root-finding
-// (a combination of bisection, secant, and inverse quadratic interpolation)
-double Solve(EqFunc function, vector<string> lhs, vector<string> rhs, double lowerLimit, double upperLimit, double errorTolerance)
+double Solve(vector<string> lhs, vector<string> rhs, double lowerLimit, double upperLimit, double errorTolerance)
 {
 	double a = lowerLimit;
 	double b = upperLimit;
 	double c = 0;
 	double d = numeric_limits<double>::max();
 	
-	double fa = function(a, lhs, rhs);
-	double fb = function(b, lhs, rhs);
+	double fa = Evaluate(lhs, a) - Evaluate(rhs, a);
+	double fb = Evaluate(lhs, b) - Evaluate(rhs, b);
 	
 	double fc = 0;
 	double s = 0;
@@ -392,7 +378,7 @@ double Solve(EqFunc function, vector<string> lhs, vector<string> rhs, double low
 		else
 			mflag = false;
 		
-		fs = function(s, lhs, rhs);
+		fs = Evaluate(lhs, s) - Evaluate(rhs, s);
 		d = c;
 		c = b;
 		fc = fb;
@@ -421,27 +407,23 @@ double Solve(EqFunc function, vector<string> lhs, vector<string> rhs, double low
 		// Keep track of iteration count, max out at 1000
 		i++;
 		if (i > 1000)
-			throw string("failed to solve equation");
+			throw "failed to solve equation";
 	}
 	
-	// return root
 	return b;
-}
-
-// Evaluate the equation for a given value
-double SolveTest(double value, vector<string> lhs, vector<string> rhs)
-{
-	x = value;
-	return Evaluate(lhs) - Evaluate(rhs);
 }
 
 void ProcessInput(string input) {
 	try
 	{
-		// Check if simple expression or equation
+		// Check if expression or equation
 		int equals = input.find("=");
 		if (equals == string::npos) 
 		{
+			// Must not have 'x' variable
+			if (input.find("x") != string::npos)
+			throw "invalid expression";
+
 			// Break up into tokens
 			vector<string> infix;
 			Tokenise(input, infix);
@@ -451,16 +433,18 @@ void ProcessInput(string input) {
 			ConvertToRPN(infix, tokens);
 		
 			// Evaluate expression
-			double result = Evaluate(tokens);
+			double result = Evaluate(tokens);		
 			
-			// Save result so it can be referenced by subsequent calculations
+			// Save and display result
 			SetLastResult(result);
-			
-			// Display result
 			cout << "= " << result << endl;
 		}
 		else
 		{
+			// Must have 'x' variable
+			if (input.find("=") == string::npos)
+				throw "invalid equation";
+
 			// Split string into lhs and rhs of equation
 			string inputL = input.substr(0, equals);
 			string inputR = input.substr(equals + 1, input.length()-equals-1);
@@ -476,16 +460,14 @@ void ProcessInput(string input) {
 			ConvertToRPN(infixR, rhs);
 
 			// Solve equation
-			double x = Solve(SolveTest, lhs, rhs, -1000, 1000, 0.00001);
+			double x = Solve(lhs, rhs, -1000, 1000, 0.00001);
 
-			// Save result so it can be referenced by subsequent calculations
+			// Save and display result
 			SetLastResult(x);
-
-			// Display result
 			cout << "x = " << x << endl;
 		}
 	}
-	catch (string errorMsg) 
+	catch (const char* errorMsg) 
 	{
 		cout << "Error: " << errorMsg << endl;
 	}
@@ -510,7 +492,7 @@ int main(int argc, char **argv)
 			string expression = buffer;
 	
 			// End input loop
-			if (expression == "exit")
+			if (expression == "exit" || expression[0] == char(27))
 				break;
 	
 			ProcessInput(expression);
